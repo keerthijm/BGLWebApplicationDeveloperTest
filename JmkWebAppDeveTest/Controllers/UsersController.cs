a0e48cf7b5320a858e6a63be795d6967853ec636
keerthijm <keerthijm@gmial.com>
2016-09-15 16:32:55 +0100

    Consuming Guthub rest apis

---
+++ JmkWebAppDeveTest/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JmkWebAppDeveTest.Controllers
+{
+    public class UsersController : Controller
+    {
+        HttpClient client;
+
+        //The URL of the WEB API Service
+        readonly string url = "https://api.github.com/users";
+
+        //The HttpClient Class, this will be used for performing 
+        //HTTP Operations, GET, POST, PUT, DELETE
+        //Set the base address and the Header Formatter
+        public UsersController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "text/html,application/json");
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");   
+        }
+
+        // GET: Users
+        public async System.Threading.Tasks.Task<ActionResult> Index()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(url);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+                var Users = JsonConvert.DeserializeObject<List<UserInfo>>(responseData);
+
+                return View(Users);
+            }         
+            return View("Error");
+        }
+    }
+}
---
+++ JmkWebAppDeveTest/Models/UserInfoModels.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace JmkWebAppDeveTest
+{
+    public class UserInfo
+    {
+        public string login { get; set; }
+        public string location { get; set; }
+        public string avatar_url { get; set; }
+    }
+}
